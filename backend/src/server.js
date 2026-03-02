c1a573d0b220d7f6b1feec978f9a611bf7b4530a
Kobe Amoh <kobexamoh@gmail.com>
2026-03-01 18:07:44 -0700

    mount the professors routers and db for initial teting

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const db = require('./db');
+const profs = require('./routes/professors')
 
 // Initialize instance of Express app and port
 const app = express();
@@ -16,6 +18,9 @@ app.get('/health', (req, res) => {
     console.log("A GET request has hit the service");
     res.send("A GET request has hit the server: this is the server response showing in the browser");
 });
+
+// mount the router for professors
+app.use('/professors', profs);
 
 // Begin the server
 app.listen(PORT, () => {
