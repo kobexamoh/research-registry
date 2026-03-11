2edd0531d04abe016f326f4703f29380f59ef2ca
Kobe Amoh <kobexamoh@gmail.com>
2026-03-10 18:07:07 -0600

    Finish initial tests

--- backend/src/__tests__/api.test.js
+++ backend/src/__tests__/api.test.js
@@ -17,6 +17,36 @@ describe('GET /professors', () => {
 });
 
 // Test 2: POST interest succeeds.
+describe('POST /interest', () => {
+    it('should create a new expression of interest and return the id of the new record in the database', async () => {
+        const res = await request(app)
+            .post('/interest')
+            .send({
+                student_name: 'Test Student',
+                student_email: 'teststudent@example.com',
+                professor_id: 1
+            }); // make POST request to the /interest endpoint with the required fields in the request body
+
+        expect(res.statusCode).toBe(201); // 201 status code indicates that server successfully processed the POST request (a request to create an expression of interest and the new resource (that is a new record) was added to the database.
+        expect(res.body.message).toBe('Expression of interest was submitted successfully.'); // confirm/check that the response body contains a success message which indicates that the expression of interest was submitted successfully.
+        expect(res.body.id).toBeDefined(); // confirm/check that the response body contains an id field - this indicates that the new record was sucessfully created in the database and that the id of the new record is being returned in the response body. 
+    });
+});
 
 
 // Test 3: POST interest fails with missing fields.
+describe('POST /interest with missing fields', () => {
+    it('should return a 400 error when the required fields are missing', async () => {
+        const res = await request(app)
+            .post('/interest')
+            .send({
+                student_name: 'Test Student'
+                // we don't include email and professor_id, the other required fields
+            });
+
+        expect(res.statusCode).toBe(400); // 400 status code means the server can't process the request due to client error (so in this case, the user didn't include all the required fields in the request body)
+        expect(res.body.error).toBeDefined(); // confirms/checks that the response body contains an error field, which should contain a message indicating what the error is (in this case, that the required fields are missing from the request body)
+
+        // TODO: a possible test for a 500 error if the DB fails?
+    });
+});
