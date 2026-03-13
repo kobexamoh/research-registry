8b3b259ba14841a77f67e5c76c8a83e198daa3e0
Kobe Amoh <kobexamoh@gmail.com>
2026-03-12 19:16:44 -0600

    Add tokens to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './tokens.css' // import global CSS styles and design tokens for consistent styling across the app
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
